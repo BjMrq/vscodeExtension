78d316977aad31fbaf2a96e36ef81a485cd4292d
Benjamin <bmarquiscom@gmail.com>
2021-02-16 21:24:22 -0500

    WIP

--- src/utils/cli.ts
+++ src/utils/cli.ts
@@ -7,3 +7,24 @@ export const openInCode = ({ applicationData }: Application) => {
     cwd: `${spockeeRoot}/${applicationData.folder}`,
   });
 };
+
+
+import { exec } from "child_process";
+import { promisify } from "util";
+import { spockeeRoot } from "../config/constants";
+
+const asyncSpawn = promisify(exec);
+
+export const cliSendActionAsync = async (
+  ...cliArguments: string[]
+): Promise<string> => {
+  const messageCli = await asyncSpawn(`spockee ${cliArguments.join(" ")}`, {
+    env: { ...process.env, SPOCKEE_ROOT: spockeeRoot },
+  });
+
+  if (messageCli.stdout) return messageCli.stdout;
+
+  if (messageCli.stderr) return messageCli.stderr;
+
+  return "An error happened with the cli";
+};
