6e9b37d0c4e358d344c8d84512c222b7bf5f448a
Benjamin <bmarquiscom@gmail.com>
2021-02-16 21:48:28 -0500

    WIP

--- src/cli/spockeeCliTree.ts
+++ src/cli/spockeeCliTree.ts
@@ -3,6 +3,20 @@
 import * as vscode from "vscode";
 
 export class SpockeeCLITree implements vscode.TreeDataProvider<any> {
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    InstalledApplication | undefined | null | void
+  > = new vscode.EventEmitter<InstalledApplication | undefined | null | void>();
+
+  readonly onDidChangeTreeData: vscode.Event<
+    InstalledApplication | undefined | null | void
+    // eslint-disable-next-line no-invalid-this
+  > = this._onDidChangeTreeData.event;
+
+  refreshWith(spockeeData: SpockeeData) {
+    this.spockeeData = spockeeData;
+    this._onDidChangeTreeData.fire();
+  }
+
   getTreeItem(element: any): vscode.TreeItem {
     return element;
   }
--- src/docker/spockeeDockerTree.ts
+++ src/docker/spockeeDockerTree.ts
@@ -8,14 +8,32 @@ import { DockerContainer } from "./DockerContainer";
 import { DockerGroup } from "./DockerGroup";
 
 export class SpockeeDockerGroupTree
-  implements vscode.TreeDataProvider<DockerGroup> {
+  implements vscode.TreeDataProvider<DockerGroup | DockerContainer> {
   constructor(private spockeeData: SpockeeData) {}
 
-  getTreeItem(element: DockerGroup): vscode.TreeItem {
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    DockerGroup | DockerContainer | undefined | null | void
+  > = new vscode.EventEmitter<
+    DockerGroup | DockerContainer | undefined | null | void
+  >();
+
+  readonly onDidChangeTreeData: vscode.Event<
+    DockerGroup | DockerContainer | undefined | null | void
+    // eslint-disable-next-line no-invalid-this
+  > = this._onDidChangeTreeData.event;
+
+  refreshWith(spockeeData: SpockeeData) {
+    this.spockeeData = spockeeData;
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTreeItem(element: DockerGroup | DockerContainer): vscode.TreeItem {
     return element;
   }
 
-  getChildren(element: DockerGroup): Thenable<any[]> {
+  getChildren(
+    element: DockerGroup | DockerContainer
+  ): Thenable<DockerGroup | DockerContainer[]> {
     if (element?.children) return Promise.resolve(element.children);
 
     const groupAndContainers = this.spockeeData.dockerGroups.map(
